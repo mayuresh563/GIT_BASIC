6fdea395300141bd73f2a81ebcadcee4c9152f16
mayuresh563 <mayuresh5603@gmail.com>
2021-07-17 19:07:18 +0530

    hello added

--- JAVA/hello.java
+++ JAVA/hello.java
@@ -1,5 +1,5 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello,This a java program");
+        System.out.println("Hello,This a java program, and this is println ");
     }
 }
